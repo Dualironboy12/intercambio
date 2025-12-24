b2d1ed2620459fdc9835b1e923054293bf4aac6e
Dualironboy12 <emi.ivan.lopez@gmail.com>
2025-12-23 21:18:51 -0600

    change to conditions

--- src/Condiciones.jsx
+++ src/Condiciones.jsx
@@ -2,7 +2,7 @@ function Condiciones() {
     return (
         <>
             <h2>Condiciones</h2>
-            <p>El intercambio se llevara a cabo el 25 de Diciembre, sujeto a cambios, lugar aun por definir. El presupuesto es de 500 pesos. La lista de deseos de cada persona se encuentra abajo.</p>
+            <p>El intercambio se llevara a cabo el 1 de Enero del 2026, en la residencia de la abuela Trini. El presupuesto es de 500 pesos. La lista de deseos de cada persona se encuentra abajo.</p>
         </>
     )
 }
